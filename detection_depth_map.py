6d7e2231e73ee6673fe9c6d11017185a300c132b
harveyrutland <hcrutland@mail.com>
2022-08-08 11:30:52 +0100

    depth detection development

--- detection_depth_map.py
+++ detection_depth_map.py
@@ -180,10 +180,10 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
   counter, fps = 0, 0
   start_time = time.time()
 
-  # Start capturing video input from the camera
-  cap = cv2.VideoCapture(camera_id)
-  cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+#   # Start capturing video input from the camera
+#   cap = cv2.VideoCapture(camera_id)
+#   cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+#   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
   # Visualization parameters
   row_size = 20  # pixels
@@ -203,44 +203,44 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
   detector = vision.ObjectDetector.create_from_options(options)
 
   # Continuously capture images from the camera and run inference
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      sys.exit(
-          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
-      )
+#   while cap.isOpened():
+#     success, image = cap.read()
+#     if not success:
+#       sys.exit(
+#           'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+#       )
 
-    counter += 1
-    image = cv2.flip(image, 1)
+   counter += 1
+   image = cv2.flip(image, 1)
 
     # Convert the image from BGR to RGB as required by the TFLite model.
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+   rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # Create a TensorImage object from the RGB image.
-    input_tensor = vision.TensorImage.create_from_array(rgb_image)
+   input_tensor = vision.TensorImage.create_from_array(rgb_image)
 
     # Run object detection estimation using the model.
-    detection_result = detector.detect(input_tensor)
+   detection_result = detector.detect(input_tensor)
 
     # Draw keypoints and edges on input image
-    image = utils.visualize(image, detection_result)
+   image = utils.visualize(image, detection_result)
 
     # Calculate the FPS
-    if counter % fps_avg_frame_count == 0:
-      end_time = time.time()
-      fps = fps_avg_frame_count / (end_time - start_time)
-      start_time = time.time()
+   if counter % fps_avg_frame_count == 0:
+       end_time = time.time()
+       fps = fps_avg_frame_count / (end_time - start_time)
+       start_time = time.time()
 
     # Show the FPS
-    fps_text = 'FPS = {:.1f}'.format(fps)
-    text_location = (left_margin, row_size)
-    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+   fps_text = 'FPS = {:.1f}'.format(fps)
+   text_location = (left_margin, row_size)
+   cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
                 font_size, text_color, font_thickness)
 
-    # Stop the program if the ESC key is pressed.
-    if cv2.waitKey(1) == 27:
-      break
-    cv2.imshow('object_detector', image)
+    # # Stop the program if the ESC key is pressed.
+    # if cv2.waitKey(1) == 27:
+    #     break
+   cv2.imshow('object_detector', image)
 
   cap.release()
   cv2.destroyAllWindows()
