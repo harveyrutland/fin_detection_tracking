6a38d81897cf7494840d2c97b7cb12984437eae7
harveyrutland <hcrutland@mail.com>
2022-08-09 00:14:16 +0100

    depth detection development

--- detection_depth_map.py
+++ detection_depth_map.py
@@ -98,10 +98,10 @@ def stereo_depth_map(rectified_pair, detection_results):
     disparity_vis = utils.visualize(disparity_color, detection_results)
     # print(detection_results.detections[0].bounding_box)
 
-    x1 = detection_results.detections[0].bounding_box[0].origin_x
-    x2 = x1 + detection_results.detections[0].bounding_box[0].width
-    y1 = detection_results.detections[0].bounding_box[0].origin_y
-    y2 = x1 + detection_results.detections[0].bounding_box[0].height
+    x1 = detection_results.detections[0].bounding_box.origin_x
+    x2 = x1 + detection_results.detections[0].bounding_box.width
+    y1 = detection_results.detections[0].bounding_box.origin_y
+    y2 = x1 + detection_results.detections[0].bounding_box.height
     # x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
     # print(detection_results)
     print('x1, x2, y1, y2', x1, x2, y1, y2)
