2433d11419f72701621e4438b39826d9e33a0fa9
harveyrutland <hcrutland@mail.com>
2022-08-08 23:04:11 +0100

    depth detection development

--- detection_depth_map.py
+++ detection_depth_map.py
@@ -96,7 +96,7 @@ def stereo_depth_map(rectified_pair, detection_results):
     disparity_color = cv2.applyColorMap(disparity_fixtype, cv2.COLORMAP_JET)
     disparity_color = utils.visualize(disparity_color, detection_result)
     # print(detection_result)
-    print(detection_result["bounding_box"])
+    print(detection_result[0])
 
 
 
