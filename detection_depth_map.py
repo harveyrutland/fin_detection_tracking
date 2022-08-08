191d041fa4c1963a297f1ea228f41768a0f4ce13
harveyrutland <hcrutland@mail.com>
2022-08-09 00:35:24 +0100

    depth detection development

--- detection_depth_map.py
+++ detection_depth_map.py
@@ -112,9 +112,10 @@ def stereo_depth_map(rectified_pair, detection_results):
         if y1 < 0:
             y1 = 0
         rect = disparity_color[y1:y1+y2, x1:x1+x2]
-        filt_rect = rect >20
-        depth_value = filt_rect.mean()
-        print('depth value', depth_value)
+        for i in rect:
+            print(i)
+        depth_value = rect.mean()
+        # print('depth value', depth_value)
     except IndexError:
         pass
 
