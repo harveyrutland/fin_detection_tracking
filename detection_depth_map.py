58276e8243b82f58242460b98c2a99f7c18a5e37
harveyrutland <hcrutland@mail.com>
2022-08-09 21:18:16 +0100

    solving depth value problem

--- detection_depth_map.py
+++ detection_depth_map.py
@@ -107,9 +107,9 @@ def stereo_depth_map(rectified_pair, detection_results):
         # y2 = y1 + detection_results.detections[0].bounding_box.height  + 60
 
         x1 = 0
-        x2 =  60
+        x2 =  10
         y1 = 0
-        y2 = 60
+        y2 = 10
         # x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
         # print(detection_results)
         print('x1, x2, y1, y2', x1, x2, y1, y2)
