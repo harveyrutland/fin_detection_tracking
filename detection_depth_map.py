f3e909e315c412505952c877c731eddba0a165d2
harveyrutland <hcrutland@mail.com>
2022-08-10 12:50:37 +0100

    solving depth value problem

--- detection_depth_map.py
+++ detection_depth_map.py
@@ -98,6 +98,7 @@ sbm = cv2.StereoBM_create(numDisparities=0, blockSize=21)
 def stereo_depth_map(rectified_pair, detection_results):
     global log
     global log_count
+   
     dmLeft = rectified_pair[0]
     dmRight = rectified_pair[1]
     disparity = sbm.compute(dmLeft, dmRight)
@@ -117,6 +118,7 @@ def stereo_depth_map(rectified_pair, detection_results):
 
     if log == False:
         angle = input('Let us wait for user input. \n') 
+        global angle
         print('loading started')
         log = True 
 
