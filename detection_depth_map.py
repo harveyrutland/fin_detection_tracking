bd76904fb534f3e69f5d56e0999c5ac0b8c561b5
harveyrutland <hcrutland@mail.com>
2022-08-10 13:06:44 +0100

    solving depth value problem

--- detection_depth_map.py
+++ detection_depth_map.py
@@ -121,7 +121,7 @@ def stereo_depth_map(rectified_pair, detection_results):
     if log == False:
         angle = input('Let us wait for user input. \n') 
         print('loading started')
-        score_dict[angle] = str(angle)
+        score_dict[angle] = angle
         log = True 
 
     if log == True:
