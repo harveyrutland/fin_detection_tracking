310dc527716f105206872d9c26e98956b50ab815
harveyrutland <hcrutland@mail.com>
2022-08-15 20:18:36 +0100

    rotation testing

--- camera_tracking.py
+++ camera_tracking.py
@@ -346,9 +346,7 @@ while True:
     if detected == True:
         ser.write(bytes(str(value), 'utf-8'))
         ser.write(b"\n")
-        print('shark in sight', value)
     else:
-        print('shark not in sight')
         value = 0.00
         ser.write(bytes(str(value), 'utf-8'))
         ser.write(b"\n")
