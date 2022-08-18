f080643b7a7ca3726141566a7a6dc810eaa5ad49
harveyrutland <hcrutland@mail.com>
2022-08-18 10:54:48 +0100

    rotation testing

--- detection_depth_map_depthtest.py
+++ detection_depth_map_depthtest.py
@@ -208,7 +208,7 @@ def stereo_depth_map(rectified_pair, detection_results):
             x1 = 0
         if y1 < 0:
             y1 = 0
-        rect = disparity_color[y1:y1+y2, score_dict = {}x1:x1+x2]
+        rect = disparity_color[y1:y1+y2,x1:x1+x2]
         ls = []
         for i in rect:
             # print(int(i.mean()))
