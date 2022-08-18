710958641de2e6deb760966ab6d34b6071501a9a
harveyrutland <hcrutland@mail.com>
2022-08-18 10:44:35 +0100

    rotation testing

--- detection_depth_map_depthtest.py
+++ detection_depth_map_depthtest.py
@@ -359,14 +359,14 @@ def run(img_left, model: str, camera_id: int, width: int, height: int, num_threa
 detection_result = None
 while True:
 
-    ser.reset_input_buffer()
+    
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2000000)
     ser.reset_input_buffer()
     if detected == True:
         ser.write(bytes(str(depth_value), 'utf-8'))
         # ser.write(b"+")
         # ser.write(bytes(str(depth_value), 'utf-8'))
-        ser.write(b"\n")
+        # ser.write(b"\n")
         # line = ser.readline().decode('utf-8').rstrip()
         print('shark in sight', depth_value)
         # print('recieving from arduino:', line)
