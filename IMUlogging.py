59d475eea9cde1d7eb23d0426064a37a66d8e2a1
harveyrutland <hcrutland@mail.com>
2022-08-19 09:33:45 +0100

    camera tracking

--- IMUlogging.py
+++ IMUlogging.py
@@ -8,7 +8,7 @@ ser.flushInput()
 while True:
     
     ser_bytes = ser.readline()
-    decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+    decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
     print(decoded_bytes)
     with open("practicle_testing_pool.csv","a") as f:
         writer = csv.writer(f,delimiter=",")
