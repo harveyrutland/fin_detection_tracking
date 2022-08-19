0b2fe0cbbb396ab711cee981a64d6df22b9bcf38
harveyrutland <hcrutland@mail.com>
2022-08-19 09:36:28 +0100

    camera tracking

--- IMUlogging.py
+++ IMUlogging.py
@@ -6,7 +6,8 @@ ser = serial.Serial('/dev/ttyACM0')
 ser.flushInput()
 
 while True:
-    
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=100000)
+    ser.flushInput()
     ser_bytes = ser.readline()
     decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
     print(decoded_bytes)
