c236e9a320f97f3f840113e7c497a8f2333b5071
harveyrutland <hcrutland@mail.com>
2022-08-19 10:05:04 +0100

    camera tracking

--- IMUlogging.py
+++ IMUlogging.py
@@ -1,6 +1,12 @@
 import serial
 import time
 import csv
+from datetime import datetime
+
+
+
+
+
 
 ser = serial.Serial('/dev/ttyACM0')
 ser.flushInput()
@@ -11,7 +17,9 @@ while True:
     ser_bytes = ser.readline()
     decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
     print(decoded_bytes)
+    timestamp = time.time()
+    dt_object = datetime.fromtimestamp(timestamp)
     with open("practicle_testing_pool.csv","a") as f:
         writer = csv.writer(f,delimiter=",")
-        writer.writerow([time.time(),decoded_bytes])
+        writer.writerow([dt_object,decoded_bytes])
    
